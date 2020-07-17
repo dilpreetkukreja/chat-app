50b880521de2f85c6801821893674131591bc46d
dilpreet kaur <dilpreet.kukreja@gmail.com>
2020-07-16 23:14:39 -0700

    second commit

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.get("/", (req, res, next) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-const PORT = 3000 || process.env.PORT;
+const PORT = process.env.PORT || 3000;
 mongoose
   .connect(DATABASE_URL, {
     useNewUrlParser: true,
